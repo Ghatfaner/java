d5e5ecddf3b930de27b8dbb4c56ef7d55d5fb8b2
Ghatfaner <ghatfaneryrazan@student.ub.ac.id>
2023-02-13 20:19:11 +0700

    first commit

---
+++ javaOOP/src/constructor/Car.java
@@ -0,0 +1,16 @@
+package constructor;
+
+public class Car {
+  String brand;
+  String model;
+  String color;
+  int year;
+  
+  Car(String brand, String model, String color, int year) {
+    this.brand = brand;
+    this.model = model;
+    this.color = color;
+    this.year = year;
+
+  }
+}
---
+++ javaOOP/src/constructor/main.java
@@ -0,0 +1,13 @@
+package constructor;
+
+public class main{
+  public static void main(String[] args) {
+    
+    Car car = new Car("Chevrolet","Camaro","black",2021);
+
+    System.out.println(car.brand);
+    System.out.println(car.model);
+    System.out.println(car.color);
+    System.out.println(car.year);
+  }
+}
